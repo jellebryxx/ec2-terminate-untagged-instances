43485fc9a8da8b74e98e80ec398d109050f2fea4
jellebryxx <83454227+jellebryxx@users.noreply.github.com>
2021-05-31 11:38:26 +0200

    Update filter-instances.py
    changes

--- filter-instances.py
+++ filter-instances.py
@@ -17,6 +17,20 @@ relay = Interface()
 to_stop = []
 to_keep = []
 
-instances = relay.get(D.instances)
+instances = filter(lambda i: i['State']['Name'] == 'running', relay.get(D.instances))
 for instance in instances:
-    print(instance)
+    try:
+        if instance['Tags'].['EnvID'] is None: 
+            to_stop.append(instance['InstanceId'])
+        else:
+            to_keep.append(instance['InstanceId'])
+    except Exception as e:
+            print('\nEC2 instance {0} not considered for termination because of a processing error: {1}'.format(instance['InstanceId'], e))
+
+print('\nFound {} instances (with tags) to keep:'.format(len(to_keep)))
+print(*[instance_id for instance_id in to_keep], sep = "\n") 
+
+print('\nFound {} instances without tags to stop:'.format(len(to_stop)))
+print(*[instance_id for instance_id in to_stop], sep = "\n") 
+
+relay.outputs.set('instanceIDs', to_stop)
